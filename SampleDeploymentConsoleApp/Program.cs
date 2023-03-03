5429c160244d8a83aca6628456f541e87b806a06
Dell <Dell@DESKTOP-2SII4NH>
2023-03-03 19:22:41 +0530

    Add project files.

---
+++ SampleDeploymentConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SampleDeploymentConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World! from shrinidhi");
+        }
+    }
+}
